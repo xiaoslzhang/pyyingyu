73ca4b817cef86de848daf3ffd0eb7e38866df1d
git <you@example.com>
2017-11-01 15:24:17 +0800

    modify some bugs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 def main():
     if sys.version_info <(3,5):
-        raise RuntimeError(python 3.0+ is require)
+        raise RuntimeError('python 3.0+ is require')
     setup(
         name = 'pyyingyu',
         version = '0.0.3',
