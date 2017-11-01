452298cc0bb4c08ec44b62027b0c99e3b0fa41f9
git <you@example.com>
2017-11-01 15:14:13 +0800

    change package

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from distutils.core import setup
+import sys
+from setuptools import setup,find_packages
 
-setup(
-    name = 'pyyingyu',
-    version = '0.0.3',
-    py_modules = ['pyyingyu'],
-)
+def main():
+    if sys.version_info <(3,5):
+        raise RuntimeError(python 3.0+ is require)
+    setup(
+        name = 'pyyingyu',
+        version = '0.0.3',
+        packages = find_packages(),
+    )
