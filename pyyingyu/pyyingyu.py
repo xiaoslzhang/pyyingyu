82c06dbd353d684f436c3a620c92be741c1732d1
git <you@example.com>
2017-11-01 14:37:24 +0800

    pyyingyu.py

---
+++ pyyingyu/pyyingyu.py
@@ -0,0 +1,60 @@
+# coding=utf-8
+
+import asyncio
+import random
+import json
+import hashlib
+
+import aiohttp
+import async_timeout
+import sys
+
+
+class BaiduTranslate:
+    lang_auto = 'auto'
+    lang_zh = 'zh'
+    lang_en = 'en'
+
+    timeout = 20
+    api_addr = 'http://fanyi-api.baidu.com/api/trans/vip/translate'
+
+    def __init__(self, loop=None):
+        self.appid = '20171009000086968'
+        self.secret = 'vZ36FjnZ91FoLJwe5NrF'
+        if loop is None:
+            self.async = False
+            self.loop = asyncio.get_event_loop()
+        else:
+            self.async = True
+            self.loop = loop
+
+    def translate(self, text, from_lang, to_lang):
+        if self.async:
+            return self._request(text, from_lang, to_lang)
+        else:
+            return self.loop.run_until_complete(self._request(text, from_lang, to_lang))
+
+    async def _request(self, text, from_lang, to_lang):
+        salt = random.randint(0, 2147483647)
+        sign = self.appid + text + str(salt) + self.secret
+        sign = hashlib.md5(sign.encode('utf-8')).hexdigest()
+        params = {'q': text, 'from': from_lang, 'to': to_lang, 'appid': self.appid, 'salt': salt, 'sign': sign}
+        async with aiohttp.ClientSession(loop=self.loop) as session:
+            with async_timeout.timeout(self.timeout, loop=self.loop):
+                async with session.post(self.api_addr,
+                                        data=params) as resp:
+                    body = await resp.read()
+                    res = json.loads(body.decode('utf-8'))
+        if 'error_code' in res and res['error_code'] != '52000':
+            raise RuntimeError(res['error_msg'])
+        return res['trans_result'][0]['dst']
+
+
+#if __name__ == '__main__':
+#    baidu = BaiduTranslate('20171009000086968', 'vZ36FjnZ91FoLJwe5NrF')
+#    #baidu = BaiduTranslate()
+#    #text = '中共北京市委北京市人民政府信访办公室网站'
+#    text = 'I like the book so much!'
+#    res = baidu.translate(text, baidu.lang_en, baidu.lang_zh)
+#    print(text)
+#    print(res)
---
+++ pyyingyu/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name = 'pyyingyu',
+    version = '0.0.3',
+    py_modules = ['pyyingyu'],
+)
